149cb1411fc879a1ead07d077466e9d2561e3015
Andre Macedo <andredekamacedo@gmail.com>
2021-07-07 19:59:40 -0300

    banca final

--- flask_156/country/api_analises.py
+++ flask_156/country/api_analises.py
@@ -1,5 +1,5 @@
 from flask_restx import Namespace, Resource, fields
-
+from datetime import date, datetime
 import mysql.connector
 import pandas as pd
 import json
@@ -18,6 +18,11 @@ api = Namespace('analises', description='Api das analises da central 156')
 class Helpers:
     def get_ano(consulta):
         return consulta.get('ano')
+        
+    def get_mes(consulta):
+        return consulta.get('mes')
+    def get_data(consulta):
+        return consulta.get('datacompleta')
 
 consulta = api.model('consulta', {
     'tipo': fields.String(required=True, description='nome do Tipo'),
@@ -72,7 +77,7 @@ class ConsultaCountAno(Resource):
     @api.doc('list_countAno')
     @api.marshal_list_with(countAno)
     def get(self, tipo, mes):
-        print(tipo, mes)
+        
         COUNTANOS.clear()
         query = "SELECT T.FK_tipo, mes, ano FROM olap_156.fato_central156 C inner join dim_tipo T inner join dim_Data D where C.FK_Tipo = T.FK_Tipo and C.FK_Data = D.FK_Data and T.FK_Tipo = {}".format(tipo)
         df = pd.read_sql(query, mydb)
@@ -83,7 +88,7 @@ class ConsultaCountAno(Resource):
             parsed = json.loads(result)
             count = len(parsed)
             countAno = {'ano': ano, 'count': count, 'tipo': tipo, 'mes': mes}
-            print(countAno)
+            
             COUNTANOS.append(countAno)
         COUNTANOS.sort(key=Helpers.get_ano)
         return COUNTANOS
@@ -92,36 +97,216 @@ varSolicitacaoMes = api.model('varsolicitacaomes', {
     'tipo': fields.String(description='nome do Tipo'),
     'mes': fields.String(description='Mês das solicitações'),
     'ano': fields.Integer(),
-    'dataCompleta': fields.DateTime(),
     'count': fields.Integer()
 
 })
 SOLICITACOESMES = []
 meses = { "Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez" }
-@api.route('/varsolicitacaomes')
 
+@api.route('/varsolicitacaomes')
 class GetVarSolicitacaoMes(Resource):
     @api.doc('list_varSolicitacaoMes')
     @api.marshal_list_with(varSolicitacaoMes)
     def get(self):
-        # if (len(SOLICITACOESMES) > 0):
-        #     return SOLICITACOESMES
+        SOLICITACOESMES = []
         query = "SELECT T.fk_tipo, tipo, mes, ano, datacompleta FROM olap_156.fato_central156 C inner join dim_tipo T inner join dim_Data D where C.FK_Tipo = T.FK_Tipo and C.FK_Data = D.FK_Data"
         df = pd.read_sql(query, mydb)
-        for ano in anos:
-            for mes in meses:
-                rdf = df.loc[(df['mes'] == mes) & (df['ano']==ano)]
-                
-                print(mes, ano)
-                print(rdf)
-
-        #     rdf = df.loc[(df['mes'].isin(meses)) & (df['ano']==ano)]
-        #     result = rdf.to_json(orient="records")
-        #     parsed = json.loads(result)
-        #     count = len(parsed)
-        #     varSolicitacaoMes = {'ano': ano, 'count': count, 'tipo': tipo, 'mes': mes}
-        #     print(varSolicitacaoMes)
+        gdf = df.groupby(['tipo','mes','ano']).size().reset_index(name='count')
+        print(gdf)
+        result = gdf.to_json(orient="records")
+        parsed = json.loads(result)
+        print(parsed)
+        for item in parsed:
+            SOLICITACOESMES.append(item)
+        #varSolicitacaoMes = {'ano': ano, 'count': count, 'tipo': tipo, 'mes': mes}
+        #print(varSolicitacaoMes)
         #     SOLICITACOESMES.append(varSolicitacaoMes)
-        # SOLICITACOESMES.sort(key=Helpers.get_ano)
+        SOLICITACOESMES.sort(key=Helpers.get_ano)
         return SOLICITACOESMES
 
+TOP10ASSUNTOS = []
+
+topAssuntos = api.model('topassuntos', {
+    'assunto': fields.String(description='nome do Assunto'),
+    'subdivisao': fields.String(description='Mês das subdivisao'),
+    'ano': fields.Integer(),
+    'count': fields.Integer()
+
+})
+
+@api.route('/topassuntos')
+class GetTopAssuntos(Resource):
+    @api.doc('list_topassuntos')
+    @api.marshal_list_with(topAssuntos)
+    def get(self):
+        TOP10ASSUNTOS = []
+        query = "SELECT assunto, subdivisao, mes, ano FROM olap_156.fato_central156 C inner join dim_assunto A inner join dim_subdivisao2 S inner join dim_Data D where S.Fk_subdivisao= C.fk_subdivisao and C.FK_Assunto = A.FK_Assunto and C.FK_Data = D.FK_Data"
+        df = pd.read_sql(query, mydb)
+        gdf = df.groupby(['assunto','subdivisao','ano']).size().reset_index(name='count')
+        mdf = gdf.sort_values(by='count', ascending=False).head(10)
+        print(mdf)
+        result = mdf.to_json(orient="records")
+        parsed = json.loads(result)
+        print(parsed)
+        for item in parsed:
+            TOP10ASSUNTOS.append(item)
+        return TOP10ASSUNTOS
+
+@api.route('/topassuntos/bairro/<bairro>')
+@api.param('<bairro>')
+class GetTopAssuntos(Resource):
+    @api.doc('list_topassuntos')
+    @api.marshal_list_with(topAssuntos)
+    def get(self, bairro):
+        TOP10ASSUNTOS = []
+        query = "SELECT assunto, subdivisao, mes, ano FROM olap_156.fato_central156 C inner join dim_assunto A inner join dim_subdivisao2 S inner join dim_Data D inner join dim_bairro2 B where B.Fk_bairro = C.Fk_bairro and S.Fk_subdivisao= C.fk_subdivisao and C.FK_Assunto = A.FK_Assunto and C.FK_Data = D.FK_Data"
+        df = pd.read_sql(query, mydb)
+        gdf = df.groupby(['assunto','subdivisao','ano']).size().reset_index(name='count')
+        mdf = gdf.sort_values(by='count', ascending=False).head(10)
+        print(mdf)
+        result = mdf.to_json(orient="records")
+        parsed = json.loads(result)
+        print(parsed)
+        for item in parsed:
+            TOP10ASSUNTOS.append(item)
+        return TOP10ASSUNTOS
+
+BAIRRO_POR_ASSUNTOS = []
+
+bairro_assunto = api.model('bairroAssunto', {
+    'assunto': fields.String(description='nome do Assunto'),
+    'count': fields.Integer()
+})
+
+@api.route('/bairroAssunto/<bairro>/<ano>')
+@api.param('<bairro>')
+@api.param('<ano>')
+class GetbairroAssunto(Resource):
+    @api.doc('list_bairroAssunto')
+    @api.marshal_list_with(bairro_assunto)
+    def get(self, bairro, ano):
+        BAIRRO_POR_ASSUNTOS = []
+        query = "SELECT assunto, bairro, mes, ano FROM olap_156.fato_central156 C inner join dim_assunto A inner join dim_bairro2 B inner join dim_Data D where B.Fk_bairro = C.fk_bairro and C.FK_Assunto = A.FK_Assunto and C.FK_Data = D.FK_Data and C.Fk_bairro = {} and ano={}".format(bairro, ano)
+        df = pd.read_sql(query, mydb)
+        gdf = df.groupby(['bairro','assunto','ano']).size().reset_index(name='count')
+        mdf = gdf.sort_values(by='count', ascending=False).head(10)
+        print(mdf)
+        result = mdf.to_json(orient="records")
+        parsed = json.loads(result)
+        print(parsed)
+        for item in parsed:
+            BAIRRO_POR_ASSUNTOS.append(item)
+        return BAIRRO_POR_ASSUNTOS
+
+@api.route('/bairroAssunto')
+class GetbairroAssunto(Resource):
+    @api.doc('list_bairroAssunto')
+    @api.marshal_list_with(bairro_assunto)
+    def get(self):
+        BAIRRO_POR_ASSUNTOS = []
+        query = "SELECT assunto, bairro, mes, ano FROM olap_156.fato_central156 C inner join dim_assunto A inner join dim_bairro2 B inner join dim_Data D where B.Fk_bairro = C.fk_bairro and C.FK_Assunto = A.FK_Assunto and C.FK_Data = D.FK_Data and ano = 2021 and B.Fk_Bairro = '30'"
+        df = pd.read_sql(query, mydb)
+        gdf = df.groupby(['assunto','bairro','ano']).size().reset_index(name='count')
+        mdf = gdf.sort_values(by='count', ascending=False).head(10)
+        print(mdf)
+        result = mdf.to_json(orient="records")
+        parsed = json.loads(result)
+        
+        for item in parsed:
+            BAIRRO_POR_ASSUNTOS.append(item)
+        return BAIRRO_POR_ASSUNTOS
+
+FAIXAS_ETARIAS_GENEROS = []
+
+faixa_etaria_genero = api.model('faixaetariagenero', {
+    'count': fields.Integer(),
+    'faixa_etaria': fields.String(description='nome do Assunto'),
+    'genero': fields.String(description='nome do Assunto')
+})
+
+@api.route('/faixaetariagenero/<assunto>')
+@api.param('<assunto>')
+class GetFaixaEtariaGenero(Resource):
+    @api.doc('list_faixaEtariaGenero')
+    @api.marshal_list_with(faixa_etaria_genero)
+    def get(self, assunto):
+        FAIXAS_ETARIAS_GENEROS = []
+        query = "SELECT genero, faixa_etaria FROM olap_156.fato_central156 C inner join dim_assunto A inner join dim_genero G inner join dim_Data D inner join dim_faixa_etaria_ibge F where F.FK_Faixa_Etaria_ibge = C.FK_Faixa_Etaria_ibge and C.Fk_genero = G.fk_genero and C.FK_Assunto = A.FK_Assunto and C.FK_Data = D.FK_Data and F.FK_Faixa_Etaria_ibge != 20 and A.Fk_assunto = {}".format(assunto)
+        df = pd.read_sql(query, mydb)
+        gdf = df.groupby(['faixa_etaria','genero']).size().reset_index(name='count')
+        mdf = gdf.sort_values(by='count', ascending=False)
+        print(mdf)
+        
+        result = mdf.to_json(orient="records")
+        parsed = json.loads(result)
+        print(parsed)
+        for item in parsed:
+            FAIXAS_ETARIAS_GENEROS.append(item)
+        return FAIXAS_ETARIAS_GENEROS
+
+@api.route('/faixaetariagenero/regional/<regional>')
+@api.param('<regional>')
+class GetFaixaEtariaGenero(Resource):
+    @api.doc('list_faixaEtariaGenero')
+    @api.marshal_list_with(faixa_etaria_genero)
+    def get(self, regional):
+        FAIXAS_ETARIAS_GENEROS = []
+        query = "SELECT genero, faixa_etaria FROM olap_156.fato_central156 C inner join dim_regional R inner join dim_genero G inner join dim_Data D inner join dim_faixa_etaria_ibge F where F.FK_Faixa_Etaria_ibge = C.FK_Faixa_Etaria_ibge and C.Fk_genero = G.fk_genero and C.FK_Regional = R.FK_Regional and C.FK_Data = D.FK_Data and F.FK_Faixa_Etaria_ibge != 20 and R.FK_Regional = {}".format(regional)
+        df = pd.read_sql(query, mydb)
+        gdf = df.groupby(['faixa_etaria','genero']).size().reset_index(name='count')
+        mdf = gdf.sort_values(by='count', ascending=False)
+        print(mdf)
+        
+        result = mdf.to_json(orient="records")
+        parsed = json.loads(result)
+        print(parsed)
+        for item in parsed:
+            FAIXAS_ETARIAS_GENEROS.append(item)
+        return FAIXAS_ETARIAS_GENEROS
+
+@api.route('/faixaetariagenero')
+class GetFaixaEtariaGenero(Resource):
+    @api.doc('list_faixaEtariaGenero')
+    @api.marshal_list_with(faixa_etaria_genero)
+    def get(self):
+        FAIXAS_ETARIAS_GENEROS = []
+        query = "SELECT genero, faixa_etaria FROM olap_156.fato_central156 C inner join dim_assunto A inner join dim_genero G inner join dim_Data D inner join dim_faixa_etaria_ibge F where F.FK_Faixa_Etaria_ibge = C.FK_Faixa_Etaria_ibge and C.Fk_genero = G.fk_genero and C.FK_Assunto = A.FK_Assunto and C.FK_Data = D.FK_Data and F.FK_Faixa_Etaria_ibge != 20 and ano = 2021"
+        df = pd.read_sql(query, mydb)
+        gdf = df.groupby(['faixa_etaria','genero']).size().reset_index(name='count')
+        mdf = gdf.sort_values(by='count', ascending=False)
+        print(mdf)
+        result = mdf.to_json(orient="records")
+        parsed = json.loads(result)
+        print(parsed)
+        for item in parsed:
+            FAIXAS_ETARIAS_GENEROS.append(item)
+        return FAIXAS_ETARIAS_GENEROS
+
+
+DATASASSUNTOPORBAIRRO = []
+
+data_assunto_por_bairro = api.model('dataAssuntoBairro', {
+    'count': fields.Integer(),
+    'datacompleta': fields.Date(),
+})
+
+@api.route('/dataAssuntoBairro/<assunto>/<bairro>')
+@api.param('<assunto>')
+@api.param('<bairro>')
+class GetDataAssuntoBairro(Resource):
+    @api.doc('list_dataAssuntoBairro')
+    @api.marshal_list_with(data_assunto_por_bairro)
+    def get(self, assunto, bairro):
+        DATASASSUNTOPORBAIRRO = []
+        query = "SELECT datacompleta, bairro, assunto FROM olap_156.fato_central156 C inner join dim_assunto A inner join dim_Data D inner join dim_bairro2 B where C.FK_assunto = A.FK_assunto and C.FK_Data = D.FK_Data and C.FK_Bairro = B.FK_Bairro and A.Fk_assunto = {} and b.FK_Bairro = {}".format(assunto, bairro)
+        df = pd.read_sql(query, mydb)
+        gdf = df.groupby(['datacompleta']).size().reset_index(name='count')
+        mdf = gdf.sort_values(by='count', ascending=False)
+        
+        result = mdf.to_json(orient="records", date_format = "iso" )
+        parsed = json.loads(result)
+       
+        for item in parsed:
+            DATASASSUNTOPORBAIRRO.append(item)
+        DATASASSUNTOPORBAIRRO.sort(key=Helpers.get_data)
+        return DATASASSUNTOPORBAIRRO
--- flask_156/demo.py
+++ flask_156/demo.py
@@ -30,26 +30,61 @@ mydb = mysql.connector.connect(
 #   CONSULTA.append(df)
 #   # print(df)
 # print(CONSULTA)
-class CountAno:
-  def __init__(self, ano, count):
-    self.ano = ano
-    self.count = count
+# class CountAno:
+#   def __init__(self, ano, count):
+#     self.ano = ano
+#     self.count = count
 
-anos = {2014,2015,2016,2017,2018,2019,2020,2021}
-CONSULTA =[]
-query = "SELECT T.FK_tipo, Mes, Ano FROM olap_156.fato_central156 C inner join dim_tipo T inner join dim_Data D where C.FK_Tipo = T.FK_Tipo and C.FK_Data = D.FK_Data and T.FK_Tipo = 5"
+# anos = {2014,2015,2016,2017,2018,2019,2020,2021}
+# CONSULTA =[]
+# query = "SELECT T.FK_tipo, Mes, Ano FROM olap_156.fato_central156 C inner join dim_tipo T inner join dim_Data D where C.FK_Tipo = T.FK_Tipo and C.FK_Data = D.FK_Data and T.FK_Tipo = 5"
+# df = pd.read_sql(query, mydb)
+# CONSULTA.append(df)
+# print(df)
+
+# print(CONSULTA)
+
+# ANOS = []
+
+# for ano in anos:
+#   rdf = df.loc[(df['Mes']=='Jan') & (df['FK_tipo'] == 5) & (df['Ano']==ano)]
+#   result = rdf.to_json(orient="records")
+#   print(result)
+#   parsed = json.loads(result)
+#   count = len(parsed)
+#   print(count)
+
+# SOLICITACOESMES = []
+# query = "SELECT assunto, subdivisao, mes, ano FROM olap_156.fato_central156 C inner join dim_assunto A inner join dim_subdivisao2 S inner join dim_Data D where S.Fk_subdivisao= C.fk_subdivisao and C.FK_Assunto = A.FK_Assunto and C.FK_Data = D.FK_Data limit 10000"
+# df = pd.read_sql(query, mydb)
+# # for ano in anos:
+# #     for mes in meses:
+
+# # print(df)
+# # mes ='Jul'
+# # ano = 2015
+
+# # class Helpers2:
+# #     def get_qntd(consulta):
+# #         return consulta.get('count')
+
+# gdf = df.groupby(['assunto','subdivisao','ano']).size().reset_index(name='count')
+# mdf = gdf.sort_values(by='count', ascending=False).head(10)
+# print(mdf)
+
+query = "SELECT assunto, genero, faixa_etaria, ano FROM olap_156.fato_central156 C inner join dim_assunto A inner join dim_genero G inner join dim_Data D inner join dim_faixa_etaria_ibge F where F.FK_Faixa_Etaria_ibge = C.FK_Faixa_Etaria_ibge and C.Fk_genero = G.fk_genero and C.FK_Assunto = A.FK_Assunto and C.FK_Data = D.FK_Data and F.FK_Faixa_Etaria_ibge != 20  limit 10000"
 df = pd.read_sql(query, mydb)
-CONSULTA.append(df)
-print(df)
 
-print(CONSULTA)
 
-ANOS = []
+gdf = df.groupby(['assunto','genero','faixa_etaria']).size().reset_index(name='count')
+mdf = gdf.sort_values(by='count', ascending=False)
+print(mdf)
 
-for ano in anos:
-  rdf = df.loc[(df['Mes']=='Jan') & (df['FK_tipo'] == 5) & (df['Ano']==ano)]
-  result = rdf.to_json(orient="records")
-  print(result)
-  parsed = json.loads(result)
-  count = len(parsed)
-  print(count)
+# Tipo
+# Assunto
+# Subdivisao
+# Genero
+# Mes
+# Ano
+#296855
+
